e47357dd76b78bf0ee0fea058b40fae3b85d47f6
rommel1125 <57905432+rommel1125@users.noreply.github.com>
2019-11-23 16:33:53 +0800

    Update navbar.php

--- ecommerce/includes/navbar.php
+++ ecommerce/includes/navbar.php
@@ -1,8 +1,8 @@
 <header class="main-header">
-  <nav class="navbar navbar-static-top">
+  <nav class="navbar navbar-static-top"  style="background-color:lightblue">
     <div class="container">
       <div class="navbar-header">
-        <a href="index.php" class="navbar-brand"><b>E-</b>TECH</a>
+        <a href="index.php" class="navbar-brand" style="color:black"><b>E-</b>TECH</a>
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
           <i class="fa fa-bars"></i>
         </button>
@@ -11,11 +11,11 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
-          <li><a href="index.php">HOME</a></li>
-          <li><a href="">ABOUT US</a></li>
-          <li><a href="">CONTACT US</a></li>
+          <li><a href="index.php" style="color:black">HOME</a></li>
+          <li><a href="" style="color:black">ABOUT US</a></li>
+          <li><a href="" style="color:black">CONTACT US</a></li>
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:black">CATEGORY <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
               <?php
              
